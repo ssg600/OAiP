632d091351028570d9f40aa04724897ffa42756b
ssg600 <96643744+ssg600@users.noreply.github.com>
2022-12-11 13:14:39 +0300

    Update zad3.c

--- Lab2/zad3.c
+++ Lab2/zad3.c
@@ -3,10 +3,10 @@
 void main() {
 	
 	int n;
-	printf ("Nomer mesyaca:");
+	printf ("Month number: ");
 	scanf ("%d", &n);
-	if (n==1 || n==2 || n==12) printf("Zima");
-	else if (n>=3 && n<=5) printf ("Vesna");
-	else if (n>=6 && n<=8) printf("Leto");
-	else printf ("Osen");
+	if (n==1 || n==2 || n==12) printf("Winter");
+	else if (n>=3 && n<=5) printf ("Spring");
+	else if (n>=6 && n<=8) printf("Summer");
+	else printf ("Autumn");
 }
